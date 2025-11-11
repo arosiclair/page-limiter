8b07be45aecbe080dc7ae8b8ec96c4a70445e5f0
arosiclair <arosiclair@gmail.com>
2025-11-10 22:16:31 -0500

    re-add margin

--- src/options.tsx
+++ src/options.tsx
@@ -196,7 +196,7 @@ const Options = () => {
     return (
         <main className="px-3 py-2">
             <div className="d-flex justify-content-between align-items-center">
-                <h2 style={{ marginBottom: 0 }}>Page Limiter - Settings</h2>
+                <h2>Page Limiter - Settings</h2>
                 <SaveIndicator isLoading={isSaving} />
             </div>
 
