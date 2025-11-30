168a0d670172bb20fb26b41008cd8d8e44084fd4
arosiclair <arosiclair@gmail.com>
2025-11-29 21:28:09 -0500

    track time in popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,30 +1,25 @@
-import React, { CSSProperties, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { getSettings } from './settings';
 import { findMatchingPattern, findMatchingGroup, getSecondsLeft } from './groups';
+import { differenceInSeconds } from 'date-fns';
 
 const Popup = () => {
-    const [currentURL, setCurrentURL] = useState<string>('');
     const [matchingAllowedPattern, setMatchingAllowedPattern] = useState<string>('');
     const [matchingGroupName, setMatchingGroupName] = useState<string>('');
     const [matchingGroupTimeLeft, setMatchingGroupTimeLeft] = useState<number>(0);
 
     useEffect(() => {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            setCurrentURL(tabs[0].url ?? '');
-        });
-    }, []);
-
-    useEffect(() => {
-        (async () => {
+        chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+            const currentURL = tabs[0].url ?? '';
             const { allowedPatterns, groups } = await getSettings();
             const allowedPattern = findMatchingPattern(allowedPatterns, currentURL);
             const matchingGroup = findMatchingGroup(groups, currentURL);
             setMatchingAllowedPattern(allowedPattern ?? '');
             setMatchingGroupName(matchingGroup?.name ?? '');
             setMatchingGroupTimeLeft(getSecondsLeft(matchingGroup));
-        })();
-    }, [currentURL]);
+        });
+    }, []);
 
     let status = '';
     let statusClass = '';
@@ -110,18 +105,44 @@ function InfinityIcon() {
     );
 }
 
-function SettingsIcon() {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            className="bi bi-gear"
-            viewBox="0 0 16 16"
-        >
-            <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0" />
-            <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z" />
-        </svg>
-    );
+let currentURL = '';
+let startTime: Date | undefined;
+
+chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+    currentURL = tabs[0].url ?? '';
+    startTimer(currentURL);
+});
+window.addEventListener('blur', endTimer);
+window.addEventListener('beforeunload', endTimer);
+
+function startTimer(url: string) {
+    if (startTime) {
+        return;
+    }
+
+    startTime = new Date();
+
+    const message: PageVisitedMessage = {
+        source: 'popup',
+        event: 'page-visited',
+        url: url,
+    };
+
+    chrome.runtime.sendMessage(message);
+}
+
+function endTimer() {
+    if (!startTime) {
+        return;
+    }
+
+    const message: AddTimeMessage = {
+        source: 'popup',
+        event: 'add-time',
+        url: currentURL,
+        secondsUsed: differenceInSeconds(new Date(), startTime),
+    };
+
+    chrome.runtime.sendMessage(message);
+    startTime = undefined;
 }
