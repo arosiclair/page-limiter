d6d414d70d3e296921ff2b667b5c3e8f5abf3d9a
Andrew Rosiclair <arosiclair@gmail.com>
2025-11-30 17:02:38 -0500

    remove boilerplate

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -2,16 +2,6 @@ import { millisecondsInSecond } from 'date-fns/constants';
 import { PageVisitedEventResult } from './service-worker';
 import { differenceInSeconds } from 'date-fns';
 
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    if (msg.color) {
-        console.log('Receive color = ' + msg.color);
-        document.body.style.backgroundColor = msg.color;
-        sendResponse('Change color to ' + msg.color);
-    } else {
-        sendResponse('Color message is none.');
-    }
-});
-
 let timeout: NodeJS.Timeout;
 let startTime: Date | undefined;
 
