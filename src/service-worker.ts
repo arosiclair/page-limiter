613183b590329fc69fda0c99f73037b1c2f9711e
arosiclair <arosiclair@gmail.com>
2025-12-07 20:46:54 -0500

    initial refactor

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,90 +1,35 @@
 import { getSettings, setGroups } from './settings';
 import { findMatchingPattern, findMatchingGroup, getCurrentDate, getSecondsLeft } from './groups';
-
-chrome.runtime.onMessage.addListener((message: ExtensionMessage, sender, sendResponse) => {
-    console.log('Message received', message);
-    switch (message.event) {
-        case 'page-visited':
-            onPageVisited(message as PageVisitedMessage).then(sendResponse);
-            return true;
-        case 'page-left':
-            // onPageLeft(message as PageLeftMessage);
-            break;
-        case 'add-time':
-            addTime(message as AddTimeMessage);
-            break;
-        default:
-            console.log('Mysterious message received', message);
-    }
-});
+import { differenceInSeconds } from 'date-fns';
 
 export type PageVisitedEventResult = {
     didMatch: boolean;
     secondsLeft: number;
 };
 
-async function onPageVisited(message: PageVisitedMessage): Promise<PageVisitedEventResult> {
-    const currentURL = message.url;
-    const { groups, allowedPatterns } = await getSettings();
+let currentMatchedGroupID: string;
+let currentPageStartTime: Date;
+let currentTimeout: NodeJS.Timeout;
 
-    const allowedPattern = findMatchingPattern(allowedPatterns ?? [], currentURL);
-    if (allowedPattern) {
-        console.log('Current page is allowed', { allowedPattern, currentURL });
-        return { didMatch: false, secondsLeft: 0 };
-    }
-
-    if (!groups) {
-        console.log('No urlGroups set', { currentURL });
-        return { didMatch: false, secondsLeft: 0 };
-    }
-
-    const matchingGroup = findMatchingGroup(groups, currentURL);
-    return {
-        didMatch: !!matchingGroup,
-        secondsLeft: getSecondsLeft(matchingGroup),
-    };
-}
-
-// async function onPageLeft(message: PageLeftMessage) {
-//     console.log('Page left', { message });
-
-//     const currentURL = message.url;
-//     const { groups } = await getSettings();
-
-//     if (!groups) {
-//         console.log('No urlGroups set', { currentURL });
-//         return;
-//     }
-
-//     const matchingGroup = findMatchingGroup(groups, currentURL);
-//     if (!matchingGroup) {
-//         console.log("Current page doesn't match", { currentURL });
-//         return;
-//     }
-
-//     const todaysHistory = matchingGroup.history[getCurrentDate()];
-//     if (!todaysHistory) {
-//         console.error('Page matched but no history for today', { currentURL, matchingGroup });
-//         return;
-//     }
-
-//     const lastHistoryEntry = todaysHistory[todaysHistory.length - 1];
-//     if (lastHistoryEntry.end) {
-//         console.log('Last history entry is already finished', { matchingGroup });
-//         return;
-//     }
-
-//     lastHistoryEntry.end = new Date().toISOString();
-//     await setURLGroups(groups);
-//     console.log('history entry finished', { matchingGroup });
-// }
-
-async function addTime(message: AddTimeMessage) {
-    const currentURL = message.url;
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+    console.log('tab activated', { activeInfo });
     const { groups, allowedPatterns } = await getSettings();
 
     if (!groups) {
-        console.log('No urlGroups set', { currentURL });
+        console.log('No urlGroups set');
+        return;
+    }
+
+    clearTimeout(currentTimeout);
+    const prevMatchingGroup = groups?.find((group) => group.id === currentMatchedGroupID);
+    const secondsUsed = differenceInSeconds(new Date(), currentPageStartTime);
+    addTime(groups, prevMatchingGroup, secondsUsed);
+
+    const tab = await chrome.tabs.get(activeInfo.tabId);
+    const currentURL = tab.url;
+
+    if (!currentURL) {
+        console.log('No URL for the current tab', { tab });
         return;
     }
 
@@ -100,9 +45,31 @@ async function addTime(message: AddTimeMessage) {
         return;
     }
 
+    const secondsLeft = getSecondsLeft(matchingGroup);
+    if (secondsLeft === 0) {
+        blockPage(activeInfo.tabId);
+        return;
+    }
+
+    currentMatchedGroupID = matchingGroup.id;
+    currentPageStartTime = new Date();
+    currentTimeout = setTimeout(() => {
+        blockPage(activeInfo.tabId);
+    }, secondsLeft * 1000);
+});
+
+async function addTime(groups: Group[], matchingGroup: Group | undefined, seconds: number) {
+    if (!matchingGroup) {
+        return;
+    }
+
     matchingGroup.secondsUsed[getCurrentDate()] =
-        (matchingGroup.secondsUsed[getCurrentDate()] ?? 0) + message.secondsUsed;
+        (matchingGroup.secondsUsed[getCurrentDate()] ?? 0) + seconds;
 
     await setGroups(groups);
-    console.log('Time added', { matchingGroup });
+    console.log('Time added', { group: matchingGroup });
+}
+
+function blockPage(tabId: number) {
+    return chrome.tabs.update(tabId, { url: 'https://0.0.0.0/' });
 }
