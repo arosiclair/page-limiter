66a2dda123a90dbf216cf4584fc0808d046c0e6e
arosiclair <arosiclair@gmail.com>
2025-12-25 10:31:41 -0500

    add toggle

--- src/components/Settings/pages/StrictModePage.tsx
+++ src/components/Settings/pages/StrictModePage.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { getSettings, setIsStrictModeEnabled as saveIsStrictModeEnabled } from '../../../settings';
 
 export default function StrictModePage() {
-    return <h1 className="title is-1">Strict Mode</h1>;
+    const [isStrictModeEnabled, setIsStrictModeEnabled] = useState(false);
+
+    useEffect(() => {
+        (async function () {
+            const settings = await getSettings();
+            setIsStrictModeEnabled(settings.isStrictModeEnabled ?? false);
+        })();
+    }, []);
+
+    return (
+        <div>
+            <label htmlFor="">
+                <span className="title is-3 mr-3">Enabled</span>
+                <input
+                    type="checkbox"
+                    style={{ width: 20, height: 20 }}
+                    checked={isStrictModeEnabled}
+                    onChange={(event) => {
+                        setIsStrictModeEnabled(event.target.checked);
+                        saveIsStrictModeEnabled(event.target.checked);
+                    }}
+                ></input>
+            </label>
+            <p>isStrictModeEnabled: {JSON.stringify(isStrictModeEnabled)}</p>
+        </div>
+    );
 }
