f3ac303cde4f7a2e41bfb43e4523241e8710b280
Andrew Rosiclair <arosiclair@gmail.com>
2026-01-09 20:21:59 -0500

    very strict mode

--- src/components/Settings/pages/StrictModePage.tsx
+++ src/components/Settings/pages/StrictModePage.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { getSettings, saveSettings } from '../../../settings';
+import { getSecondsLeft } from '../../../groups';
 
 export default function StrictModePage() {
     const [isStrictModeEnabled, setIsStrictModeEnabled] = useState(false);
+    const [hasExpiredGroups, setHasExpiredGroups] = useState(false);
+    const shouldRestrictChanges = isStrictModeEnabled && hasExpiredGroups;
 
     useEffect(() => {
         (async function () {
             const settings = await getSettings();
             setIsStrictModeEnabled(settings.isStrictModeEnabled ?? false);
+            setHasExpiredGroups(
+                settings.groups.some(
+                    (group) => group.timelimitSeconds !== 0 && getSecondsLeft(group) === 0
+                )
+            );
         })();
     }, []);
 
@@ -22,17 +30,23 @@ export default function StrictModePage() {
                         style={{ width: 20, height: 20 }}
                         checked={isStrictModeEnabled}
                         onChange={(event) => {
+                            if (shouldRestrictChanges) {
+                                return;
+                            }
+
                             setIsStrictModeEnabled(event.target.checked);
                             saveSettings({ isStrictModeEnabled: event.target.checked });
                         }}
+                        disabled={shouldRestrictChanges}
                     ></input>
                 </label>
             </div>
             <ul className="pl-5" style={{ listStyle: 'initial' }}>
-                <li>Restricts changes to the allow list after any group is expired</li>
+                <li>Restricts changes to the allow list while any group is expired</li>
                 <li>Restricts changes to any group above an expired group</li>
                 <li>Restricts changes to an expired group</li>
                 <li>Restricts changes to the order of groups below an expired group</li>
+                <li>Cannot be disabled while any group is expired</li>
             </ul>
         </div>
     );
