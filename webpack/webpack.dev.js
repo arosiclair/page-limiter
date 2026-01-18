28a0b2a86b3113669ac75fbf988961c28484b246
arosiclair <arosiclair@gmail.com>
2026-01-17 21:45:48 -0500

    add dev icon

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
     devtool: 'inline-source-map',
-    mode: 'development'
+    mode: 'development',
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: '.', to: '../', context: 'public-dev', force: true }],
+            options: {},
+        }),
+    ],
 });
