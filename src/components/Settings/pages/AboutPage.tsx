99727543824e60a42eb6900ed508259f875e78d0
arosiclair <arosiclair@gmail.com>
2026-01-18 12:23:05 -0500

    add details and links

--- src/components/Settings/pages/AboutPage.tsx
+++ src/components/Settings/pages/AboutPage.tsx
@@ -6,5 +6,39 @@ declare global {
 }
 
 export default function AboutPage() {
-    return <h3 className="title is-3">Version {__VERSION__}</h3>;
+    return (
+        <div>
+            <div className="content">
+                <h3 className="title is-3">Version {__VERSION__}</h3>
+                <ul>
+                    <li>
+                        Created by{' '}
+                        <a href="https://arosiclair.com" target="_blank" rel="noopener noreferrer">
+                            Andrew Rosiclair
+                        </a>
+                    </li>
+                    <li>
+                        Source code on{' '}
+                        <a
+                            href="https://github.com/arosiclair/page-limiter"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            GitHub
+                        </a>
+                    </li>
+                    <li>
+                        Have an issue? Report it on{' '}
+                        <a
+                            href="https://github.com/arosiclair/page-limiter/issues"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            GitHub
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
