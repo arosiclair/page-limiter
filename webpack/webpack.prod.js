a650b0eea3066c980cfb817abcb5ec388a0066b3
Andrew Rosiclair <arosiclair@gmail.com>
2026-02-12 20:30:29 -0500

    add beta config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,19 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const plugins = [];
+
+if (process.env.BETA) {
+    plugins.push(
+        new CopyPlugin({
+            patterns: [{ from: '.', to: '../', context: 'public-beta', force: true }],
+            options: {},
+        }),
+    );
+}
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    plugins,
 });
